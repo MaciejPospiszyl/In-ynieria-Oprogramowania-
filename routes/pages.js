e48893aef9c869ab3b380d700288598302b2d910
Maciej Pospiszyl <xmadziej@gmail.com>
2021-04-19 12:15:55 +0200

    small routing fix

--- routes/pages.js
+++ routes/pages.js
@@ -38,7 +38,7 @@ router.get('/easy', authController.isLoggedIn, (req, res) => {
         })
     }
     else {
-        res.redirect('login')
+        res.redirect('logowanie')
     }
 })
 
@@ -51,7 +51,7 @@ router.get('/ranking', authController.isLoggedIn, authController.getLeaderboard,
         })
     }
     else {
-        res.redirect('login')
+        res.redirect('logowanie')
     }
 })
 
@@ -63,7 +63,7 @@ router.get('/medium', authController.isLoggedIn, (req, res) => {
     })
     }
     else{
-        res.redirect('login')
+        res.redirect('logowanie')
     }
 })
 
@@ -75,7 +75,7 @@ router.get('/hard', authController.isLoggedIn, (req, res) => {
     })
     }
     else{
-        res.redirect('login')
+        res.redirect('logowanie')
     }
 })
 
